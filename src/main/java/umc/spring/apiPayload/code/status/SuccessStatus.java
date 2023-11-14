9ae4ad7f09d0e8481f128115834e72d0a63405da
Eunjin.Kim <jennyeunjin@khu.ac.kr>
2023-11-14 22:20:43 +0900

    Add ApiResponse, status enum

---
+++ src/main/java/umc/spring/apiPayload/code/status/SuccessStatus.java
@@ -0,0 +1,37 @@
+package umc.spring.apiPayload.code.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import umc.spring.apiPayload.code.BaseCode;
+import umc.spring.apiPayload.code.ReasonDTO;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    // 일반 응답
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .code(code)
+                .message(message)
+                .isSuccess(true).build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .isSuccess(true)
+                .build();
+    }
+}
