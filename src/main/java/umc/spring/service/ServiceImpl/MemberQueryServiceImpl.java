ad4ab683ee80c8c0158d2f7f48fa864a3370c825
Eunjin.Kim <jennyeunjin@khu.ac.kr>
2023-12-04 14:10:27 +0900

    Feature/7 회원 리뷰 목록 조회 API 구현

--- src/main/java/umc/spring/service/ServiceImpl/MemberQueryServiceImpl.java
+++ src/main/java/umc/spring/service/ServiceImpl/MemberQueryServiceImpl.java
@@ -1,9 +1,14 @@
 package umc.spring.service.ServiceImpl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.spring.domain.Member;
+import umc.spring.domain.review.Review;
 import umc.spring.repository.MemberRepository;
+import umc.spring.repository.ReviewRepository;
 import umc.spring.service.MemberQueryService;
 
 @Service
@@ -11,8 +16,17 @@ import umc.spring.service.MemberQueryService;
 @Transactional(readOnly = true)
 public class MemberQueryServiceImpl implements MemberQueryService {
     private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
     @Override
     public boolean isExistMember(Long id) {
         return memberRepository.existsById(id);
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long memberId, Integer page) {
+        Member member = memberRepository.findById(memberId).get();
+        Page<Review> memberPage = reviewRepository.findAllByMember(member, PageRequest.of(page,10));
+
+        return memberPage;
     }
 }
--- src/main/java/umc/spring/web/dto/ReviewResponseDTO.java
+++ src/main/java/umc/spring/web/dto/ReviewResponseDTO.java
@@ -44,4 +44,29 @@ public class ReviewResponseDTO {
         LocalDate createdAt;
 
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyReviewPreViewListDTO{
+        List<MyReviewPreViewDTO> myReviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyReviewPreViewDTO{
+        String ownerNickname;
+        String restaurantName;
+        Float score;
+        String body;
+        LocalDate createdAt;
+    }
 }
